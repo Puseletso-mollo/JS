fdf53f9d46e81c27f3a3d8799d0b92bfa04b518a
Puseletso-mollo <puseletsomollo26@gmail.com>
2023-11-07 12:42:25 +0200

    add modified getters and setters

--- getters+setters.js
+++ getters+setters.js
@@ -1,18 +1,32 @@
 const student = {
-    //data property
+    //data property attribute
     firstName: 'Puseletso',
-    // accessor property(getter)
-    get getName() {
-        return this.firstName;
-        
+    surname: "Mollo",
+    age: 18,
+    contact: "0724781414",
+ 
+    
+//accessor property(getter)
+get getName (){
+    return this.firstName;
+},
+
+get getDetails(){
+    return[this.firstName, this.surname]
+},
+
+
+//accessor property(setter)
+set changeName(newName){
+    this.firstName=newName;
 }
 }
 
-console.log(student.firstName);
+//call all information
+console.log(student.getDetails)
 
-console.log(student.getName);
+console.log(student.firstName)
+console.log(student.getName)
 
-//Assign a new name to first name
-student.changeName="Mmamokete"
 
-console.log(student.firstName);
+student.changeName="Ashante";
