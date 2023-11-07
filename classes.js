1ae49fd3f2b7bf147d00fd0a0f35004e6664d56b
Puseletso-mollo <puseletsomollo26@gmail.com>
2023-11-07 14:24:39 +0200

    added classes

---
+++ classes.js
@@ -0,0 +1,18 @@
+class vehicle {
+    constructor (make, name, engine){
+        this.make = make;
+        this.name = name;
+        this.engine = engine;
+
+    }
+
+    get getName(){
+        return (`The name of this vehicle is ${this.name}`)
+    };
+}
+     let bike = new vehicle("Kawasaki", "suzuki", "1340c");
+     let car = new vehicle("yaris", "Toyota", "VVT1");
+     let truck = new vehicle("Volvo", "Mercedes Benz", "C63")
+
+     console.log(`${bike.getName}`)
+     console.log(`${bike.name}`)
